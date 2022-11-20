a57c925bba03aea244660f86556005b55a2da48b
Prabhjot Singh <prabhjotsingh394@gmail.com>
2022-11-20 11:03:33 +0530

    Importing AngularFire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAYSYQ-FdD2IhURHNSE8Cgn9R5NLFSXG8w",
+    authDomain: "clips-97cd4.firebaseapp.com",
+    projectId: "clips-97cd4",
+    storageBucket: "clips-97cd4.appspot.com",
+    messagingSenderId: "506801986780",
+    appId: "1:506801986780:web:31f27b2d1c87a910d76819"
+  }
 };
